6b50c5ab7942f30dbb89e24e0f0db49640d117b7
Julian Rivera Jr <julianriverajr@gmail.com>
2020-11-29 22:25:39 -0600

    added reamining necessary code for deployment

--- server.js
+++ server.js
@@ -17,6 +17,8 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MongoDB_URI || "mongodb://localhost/budgetdb", {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false
 });
 
